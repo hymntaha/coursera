f1fd938d4d6d8f13b89abe72f9510b2dfb58c5f4
taha <tahauygun@gmail.com>
2018-06-17 23:45:56 -0500

    money change

--- week_3/change.py
+++ week_3/change.py
@@ -2,9 +2,18 @@
 import sys
 
 def get_change(m):
-    #write your code here
-    return m
+    coins = [1,5,10]
+    count, n = len(coins) + 1, int(m) + 1
+    table = [[0] * n for x in range(count)]
+
+    for j in range(1, n):
+        table[0][j] = float('inf')
+    for i in range(1, count):
+        for j in range(1, n):
+            aC = table[i][j - coins[i - 1]] if j - coins[i - 1] >= 0 else float('inf')
+            table[i][j] = min(table[i - 1][j], 1 + aC)
+    return table[count - 1][n - 1]
 
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
+    m = input()
     print(get_change(m))
